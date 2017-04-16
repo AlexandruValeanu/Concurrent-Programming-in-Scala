42d6d5ee777b96276605369944f92eff1d9b985a
Alexandru Văleanu <alexandru.valeanu@oriel.ox.ac.uk>
2017-04-16 14:33:58 +0300

    update channels functions

---
+++ src/concurrent_programming/channels/ChannelsFunctions.scala
@@ -0,0 +1,22 @@
+package concurrent_programming.channels
+
+import io.threadcso.{!, ?, PROC, proc}
+
+object ChannelsFunctions {
+  def copy[T](in: ?[T], out: ![T]): PROC = proc{
+    while (true){
+      val x = in?()
+      out!x
+    }
+    in.closeIn(); out.closeOut()
+  }
+
+  def console[T](in: ?[T]): PROC = proc{
+    while (true){
+      println(in?())
+    }
+    in.closeIn()
+  }
+
+
+}
